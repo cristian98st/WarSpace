6b9d03a54d03208df9efc370cabe84ee0b9ccd19
cristian98st <cris_st7@yahoo.com>
2019-01-17 01:31:28 +0200

    Client 90%, Server 50%

--- client.cpp
+++ client.cpp
@@ -1,1 +1,175 @@
 //client code
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctime>
+
+using namespace std;
+
+extern int errno;
+
+int port = 7777;
+
+void validateRole(int validRole, char (&buffer)[1024], int &role)
+{
+    while (validRole == 0)
+    {
+        if (strcmp(buffer, "general") == 0)
+        {
+            role = 0;
+            validRole = 1;
+        }
+        else if (strcmp(buffer, "commander") == 0)
+        {
+            role = 1;
+            validRole = 1;
+        }
+        else if (strcmp(buffer, "wrrz") == 0)
+        {
+            role = 2;
+            validRole = 1;
+        }
+        else
+        {
+            printf("Unvalid role was chosen. Please enter again your role:\n");
+            bzero(buffer, 1024);
+            read(0, buffer, 1024);
+        }
+    }
+}
+
+void availRole(char (&buffer)[1024], int &role, int sd)
+{
+    int valid = 0;
+    while (valid == 0)
+    {
+        if (strcmp(buffer, "1") != 0 && strcmp(buffer, "2") != 0 && strcmp(buffer, "3") != 0)
+        {
+            printf("%s\n", buffer);
+            bzero(buffer, 1024);
+            read(0, buffer, 0);
+            validateRole(0, buffer, role); // is the role existent?
+            write(sd, buffer, 1024);       // send it to the server
+            bzero(buffer, 1024);
+            read(sd, buffer, 1024); // get the answer
+        }
+        else
+            valid = 1;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int sd;
+    struct sockaddr_in server;
+    char buffer[1024];
+
+    int wins = 0, valid = 0;
+    int fightResult;
+    int role; // 0-general, 1-commander, 2-wrrz
+
+    srand(time(NULL));
+
+    if (argc != 2)
+    {
+        printf("Too few argumets.\n");
+        return -1;
+    }
+
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("[-]Error at socket()\n");
+        return errno;
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = inet_addr(argv[1]);
+
+    if (connect(sd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1)
+    {
+        perror("[-]Error at connection.\n");
+        return errno;
+    }
+
+    bzero(buffer, 1024);
+    printf("Who are you, stranger?\n");
+    fflush(stdout);
+    read(0, buffer, 1024); // cin role
+    validateRole(0, buffer, role);
+
+    if ((write(sd, buffer, 1024)) == -1) //send role
+    {
+        perror("[-] Error at sending to the server.\n");
+        return errno;
+    }
+
+    bzero(buffer, 1024);
+    read(sd, buffer, 1024); // get response
+
+    availRole(buffer, role, sd); // check if the response was good
+                                 // at this point a role has been assigned to the client
+    if (role == 1)               // commander
+    {
+        do
+        {
+            bzero(buffer, 1024);
+            read(sd, buffer, 1024);
+            if (strcmp(buffer, "fight") == 0)
+            {
+                printf("A wrrz spaceship has arrived. We have been ordered to counter-attack it. To battle!\n");
+                fightResult = rand() % 101;
+                if (fightResult % 2 == 0)
+                {
+                    printf("Shields are down! Auxiliar shields are inoperative. Critical damage! Abandon the ship! \n You have died on duty!\n");
+                    return 0;
+                }
+                else
+                {
+                    wins++;
+                    if (wins > 2)
+                        printf("Congratsulations! You have won your %d fight against the hideous Wrrz. You are a champion!", wins);
+                    printf("Congratsulations! You have won your %d fight against the hideous Wrrz.", wins);
+                    connect(sd, (struct sockaddr *)&server, sizeof(struct sockaddr));
+                    bzero(buffer, 1024);
+                    fflush(stdout);
+                    buffer[0] = wins + '0';
+                    write(sd, buffer, 1024); // sned the role as the number of wins
+                }
+            }
+        } while (strcmp(buffer, "fight") == 0);
+    }
+
+    else if (role == 2) // wrrz
+    {
+        printf("You have done your undespicable duty. You have attacked the enemy human fleet but you have run away before you could see the damage you have done... Despiteful...\n");
+    }
+
+    else //general
+    {
+        int alive=3;
+
+        while(alive)
+        {
+            bzero(buffer, 1024);
+            read(sd, buffer, 1024);
+
+            if();
+        }
+    }
+
+    return 0;
+}
--- server.cpp
+++ server.cpp
@@ -1,1 +1,137 @@
 //server code
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PORT 7777
+
+extern int errno;
+
+char *conv_addr(struct sockaddr_in address)
+{
+    static char str[25];
+    char port[7];
+
+    /* adresa IP a clientului */
+    strcpy(str, inet_ntoa(address.sin_addr));
+    /* portul utilizat de client */
+    bzero(port, 7);
+    sprintf(port, ":%d", ntohs(address.sin_port));
+    strcat(str, port);
+    return (str);
+}
+
+int main()
+{
+    int sockfd;
+    struct sockaddr_in serverAddr;
+
+    int newSocket;
+    struct sockaddr_in newAddr;
+
+    socklen_t addr_size;
+
+    char buffer[1024];
+
+    pid_t roleP;   // process treats the client
+    pid_t acceptP; // accepts clients
+
+    int gameSocket[2]; // communication between the main proces (the game) and the client's process
+    // [0]-the client | [1]-the game
+
+    int generalSocket[2]; // communication between the main process and the general
+    // [0]-the general | [1]-the game
+
+    // game parameters:
+    int clientfd[10];
+    int status; // for wait()
+
+    if (socketpair(AF_UNIX, SOCK_STREAM, 0, gameSocket) < 0) // client socketpair
+    {
+        perror("[] Error at creating game-client socket. \n");
+        return errno;
+    }
+
+    if (socketpair(AF_UNIX, SOCK_STREAM, 0, generalSocket) < 0) // general socketpair
+    {
+        perror("[] Error at creating game-general socket. \n");
+        return errno;
+    }
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("[-] Error at socket creation. \n");
+        return errno;
+    }
+    printf("[+] Server socket created. \n");
+
+    bzero(&serverAddr, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+    {
+        perror("[-] Error at binding. \n");
+        return errno;
+    }
+    printf("[+] Bind succesful to port %d. \n", PORT);
+
+    if (listen(sockfd, 10) == 0)
+    {
+        printf("Listening...\n");
+    }
+    else
+    {
+        perror("[-] Error at listening. \n");
+        return errno;
+    }
+
+    if((acceptP = fork()) < 0)
+    {
+        perror("[-] Error at first fork(). \n");
+        return errno;
+    }
+    else if(acceptP !=0) // main process
+    {
+        // commui=nication through socketpairs
+    }
+    else
+    {
+        while (1)
+        {
+            newSocket = accept(sockfd, (struct sockaddr *)&newAddr, &addr_size);
+            if (newSocket < 0)
+            {
+                perror("[-] Error at accepting. \n");
+                return errno;
+            }
+            
+            printf("[server] Ther client with the %d descriptor, from the address %s has connected. \n", newSocket, conv_addr(newAddr));
+
+            if((roleP = fork()) < 0)
+            {
+                perror("[-] Error at second fork(). \n");
+                return errno;
+            }
+            else if(roleP == 0) // client's process
+            {
+                int chosen = 0;
+            }
+        }
+    }
+
+    return 0;
+}
